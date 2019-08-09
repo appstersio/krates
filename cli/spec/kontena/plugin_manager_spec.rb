a542d8a4f6569434e0c450c9356add3b88fbbb20
Pavel Tsurbeleu <pavel.tsurbeleu@me.com>
2019-08-08 22:13:17 -0700

    Fix issues with 'plugin_manager_spec.rb' and 'plugin_manager/loader.rb' due to renaming

--- cli/spec/kontena/plugin_manager_spec.rb
+++ cli/spec/kontena/plugin_manager_spec.rb
@@ -7,12 +7,12 @@ describe Kontena::PluginManager do
   before(:each) { subject.init }
   describe '#load_plugins' do
     it 'includes hello plugin' do
-      expect(subject.plugins.any?{ |p| p.name == 'kontena-plugin-hello' }).to be_truthy
+      expect(subject.plugins.any?{ |p| p.name == 'krates-plugin-hello' }).to be_truthy
     end
 
     it 'allows plugin to register as a sub-command' do
-      plugins = subject.init
-      main = Kontena::MainCommand.new(File.basename($0))
+      plugins, basename = subject.init, File.basename($0)
+      main = Kontena::MainCommand.new(basename)
       expect {
         main.run(['hello'])
       }.to raise_error(Clamp::HelpWanted)
@@ -26,36 +26,36 @@ describe Kontena::PluginManager do
 
     it 'returns true if spec dependency > than MIN_CLI_VERSION' do
       spec = Gem::Specification.new do |s|
-        s.name        = 'kontena-plugin-foo'
+        s.name        = 'krates-plugin-foo'
         s.version     = '0.1.0'
-        s.add_runtime_dependency 'kontena-cli', '>= 0.16.0'
+        s.add_runtime_dependency 'krates', '>= 1.6.0'
       end
       expect(Kontena::PluginManager::Loader.new.send(:spec_has_valid_dependency?, spec)).to be_truthy
     end
 
     it 'returns true if spec dependency > than MIN_CLI_VERSION and is prerelease' do
       spec = Gem::Specification.new do |s|
-        s.name        = 'kontena-plugin-foo'
+        s.name        = 'krates-plugin-foo'
         s.version     = '0.1.0'
-        s.add_runtime_dependency 'kontena-cli', '>= 0.16.0.pre2'
+        s.add_runtime_dependency 'krates', '>= 1.6.0.pre2'
       end
       expect(Kontena::PluginManager::Loader.new.send(:spec_has_valid_dependency?, spec)).to be_truthy
     end
 
     it 'returns false if spec dependency < than MIN_CLI_VERSION' do
       spec = Gem::Specification.new do |s|
-        s.name        = 'kontena-plugin-foo'
+        s.name        = 'krates-plugin-foo'
         s.version     = '0.1.0'
-        s.add_runtime_dependency 'kontena-cli', '>= 0.15.0'
+        s.add_runtime_dependency 'krates', '>= 0.15.0'
       end
       expect(Kontena::PluginManager::Loader.new.send(:spec_has_valid_dependency?, spec)).to be_falsey
     end
 
     it 'returns false if spec dependency < than MIN_CLI_VERSION and is prerelease' do
       spec = Gem::Specification.new do |s|
-        s.name        = 'kontena-plugin-foo'
+        s.name        = 'krates-plugin-foo'
         s.version     = '0.1.0'
-        s.add_runtime_dependency 'kontena-cli', '>= 0.15.0.beta1'
+        s.add_runtime_dependency 'krates', '>= 0.15.0.beta1'
       end
       expect(Kontena::PluginManager::Loader.new.send(:spec_has_valid_dependency?, spec)).to be_falsey
     end
