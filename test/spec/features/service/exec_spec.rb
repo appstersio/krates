0bf39b615b92c96d926f016a5991866f7d956ef0
Pavel Tsurbeleu <pavel.tsurbeleu@me.com>
2019-08-20 22:26:04 -0700

    Fix up 'service' feature specs to refer new CLI

--- test/spec/features/service/exec_spec.rb
+++ test/spec/features/service/exec_spec.rb
@@ -3,43 +3,43 @@ require 'spec_helper'
 describe 'service exec' do
 
   before(:all) do
-    run!("kontena service create test-1 redis:3.0")
-    run!("kontena service deploy test-1")
+    run!("krates service create test-1 redis:3.0")
+    run!("krates service deploy test-1")
   end
 
   after(:all) do
-    run("kontena service rm --force test-1")
+    run("krates service rm --force test-1")
   end
 
   it 'runs a command inside a service' do
-    k = kommando("kontena service exec test-1 -- hostname -s")
+    k = kommando("krates service exec test-1 -- hostname -s")
     expect(k.run).to be_truthy
     expect(k.code).to be_zero, k.out
     expect(k.out).to eq("test-1-1\r\n")
   end
 
   it 'exits with error if command fails' do
-    k = kommando("kontena service exec test-1 -- ls -l /nonexist")
+    k = kommando("krates service exec test-1 -- ls -l /nonexist")
     k.run
     expect(k.code).to_not eq 0
     expect(k.out).to include("/nonexist: No such file or directory")
   end
 
   it 'exits with command error' do
-    k = kommando("kontena service exec --shell test-1 exit 32")
+    k = kommando("krates service exec --shell test-1 exit 32")
     k.run
     expect(k.code).to eq(32)
   end
 
   it 'returns an error if command not found' do
-    k = kommando("kontena service exec test-1 thisdoesnotexist")
+    k = kommando("krates service exec test-1 thisdoesnotexist")
     k.run
     expect(k.code).to eq(126)
   end
 
   describe '--tty' do
     it 'runs a command inside a service' do
-      k = kommando("kontena service exec -it test-1 sh")
+      k = kommando("krates service exec -it test-1 sh")
 
       k.out.on("#") do
         k.in << "ls -la /\r"
@@ -52,7 +52,7 @@ describe 'service exec' do
     end
 
     it 'runs a command with tty control input' do
-      k = kommando("kontena service exec -it test-1 sh")
+      k = kommando("krates service exec -it test-1 sh")
 
       k.out.on("#") do
         k.in << "sleep 10 && echo ok\r"
@@ -68,7 +68,7 @@ describe 'service exec' do
     end
 
     it 'runs a command with non-ascii input' do
-      k = kommando("kontena service exec -it test-1 sh")
+      k = kommando("krates service exec -it test-1 sh")
 
       k.out.on("#") do
         k.in << "echo f\u00e5\u00e5 | LANG=C.UTF-8 rev\r"
@@ -83,8 +83,8 @@ describe 'service exec' do
     end
 
     # RuntimeError : stdin read JSON::GeneratorError: partial character in source, but hit end
-    pending 'runs a command with chunked UTF-8 input' do
-      k = kommando("kontena service exec -it test-1 sh")
+    it 'runs a command with chunked UTF-8 input', :skip => 'RuntimeError : stdin read' do
+      k = kommando("krates service exec -it test-1 sh")
 
       k.out.on("#") do
         data = "echo f\u00e5\u00e5\r"
@@ -108,14 +108,14 @@ describe 'service exec' do
 
   describe '--interactive' do
     it 'runs a command with piped stdin' do
-      k = kommando("$ echo beer | kontena service exec -i test-1 rev")
+      k = kommando("$ echo beer | krates service exec -i test-1 rev")
       expect(k.run).to be_truthy
       expect(k.code).to be_zero, k.out
       expect(k.out).to eq('reeb')
     end
 
     it 'runs a command with piped non-ascii stdin' do
-      k = kommando("$ echo f\u00e5\u00e5 | kontena service exec -i test-1 -- sh -c 'LANG=C.UTF-8 rev'")
+      k = kommando("$ echo f\u00e5\u00e5 | krates service exec -i test-1 -- sh -c 'LANG=C.UTF-8 rev'")
       expect(k.run).to be_truthy
       expect(k.code).to be_zero, k.out
       expect(k.out).to eq("\u00e5\u00e5f")
@@ -124,15 +124,15 @@ describe 'service exec' do
 
   context 'with multiple instances' do
     before(:all) do
-      run("kontena service scale test-1 2")
+      run("krates service scale test-1 2")
     end
 
     after(:all) do
-      run("kontena service scale test-1 1")
+      run("krates service scale test-1 1")
     end
 
     it 'runs a command inside a service on a given instances' do
-      k = kommando("kontena service exec --instance 2 test-1 -- hostname -s")
+      k = kommando("krates service exec --instance 2 test-1 -- hostname -s")
       expect(k.run).to be_truthy
       expect(k.code).to be_zero, k.out
       expect(k.out).to eq("test-1-2\r\n")
@@ -140,21 +140,21 @@ describe 'service exec' do
 
 
     it 'runs a command on every instance with --all' do
-      k = kommando("kontena service exec --all --silent test-1 -- hostname -s")
+      k = kommando("krates service exec --all --silent test-1 -- hostname -s")
       expect(k.run).to be_truthy
       expect(k.code).to be_zero, k.out
       expect(k.out).to eq("test-1-1\r\ntest-1-2\r\n")
     end
 
     it 'fails early if running a command on every instances with --all fails' do
-      k = kommando("kontena service exec --all --silent --shell test-1 -- hostname -s && false")
+      k = kommando("krates service exec --all --silent --shell test-1 -- hostname -s && false")
       expect(k.run).to be_truthy
       expect(k.code).to_not be_zero, k.out
       expect(k.out).to eq("test-1-1\r\n")
     end
 
     it 'keeps going if running a command on every instances with --all --skip' do
-      k = kommando("kontena service exec --all --skip --silent --shell test-1 -- hostname -s && false")
+      k = kommando("krates service exec --all --skip --silent --shell test-1 -- hostname -s && false")
       expect(k.run).to be_truthy
       expect(k.code).to_not be_zero, k.out
       expect(k.out).to eq("test-1-1\r\ntest-1-2\r\n")
