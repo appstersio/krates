431eb3a59655a5be817ca3a5bc65403229174289
Pavel Tsurbeleu <pavel.tsurbeleu@me.com>
2019-08-22 23:44:39 -0700

    Fix up 'stack/registry' feature specs to refer new CLI

--- test/spec/features/stack/registry/pull_spec.rb
+++ test/spec/features/stack/registry/pull_spec.rb
@@ -4,7 +4,7 @@ describe 'stack registry' do
   context 'pull' do
     context 'when the stack does not exist' do
       it 'exits with an error' do
-        k = run 'kontena stack registry pull --no-cache kontena/doesnot-exist'
+        k = run 'krates stack registry pull --no-cache kontena/doesnot-exist'
         expect(k.code).not_to be_zero
         expect(k.out).to match /not found/
       end
@@ -13,7 +13,7 @@ describe 'stack registry' do
     context 'when the stack exists' do
       context 'without version number' do
         it 'displays the YAML' do
-          k = run! 'kontena stack registry pull --no-cache kontena/ingress-lb'
+          k = run! 'krates stack registry pull --no-cache kontena/ingress-lb'
           expect(k.out.lines.find { |l| l.start_with?('stack: kontena/ingress-lb')}).not_to be_nil
           expect(k.out.lines.find { |l| l.start_with?('variables:')}).not_to be_nil
           expect(k.out.lines.find { |l| l.start_with?('services:')}).not_to be_nil
@@ -23,7 +23,7 @@ describe 'stack registry' do
 
       context 'with version number' do
         it 'displays the YAML for that version' do
-          k = run! 'kontena stack registry pull --no-cache kontena/ingress-lb:0.1.0'
+          k = run! 'krates stack registry pull --no-cache kontena/ingress-lb:0.1.0'
           expect(k.out.lines.find { |l| l.start_with?('stack: kontena/ingress-lb')}).not_to be_nil
           expect(k.out.lines.find { |l| l.start_with?('variables:')}).not_to be_nil
           expect(k.out.lines.find { |l| l.start_with?('services:')}).not_to be_nil
--- test/spec/features/stack/registry/search_spec.rb
+++ test/spec/features/stack/registry/search_spec.rb
@@ -3,19 +3,19 @@ require 'spec_helper'
 describe 'stack registry' do
   context 'search' do
     it 'shows a list of stacks' do
-      k = run! 'kontena stack registry search'
+      k = run! 'krates stack registry search'
       expect(k.out.lines.size > 20).to be_truthy
       expect(k.out).to match /kontena\/ingress-lb/
     end
 
     it 'shows a list of stacks filtered by name' do
-      k = run! 'kontena stack registry search kontena/ingress-lb'
+      k = run! 'krates stack registry search kontena/ingress-lb'
       expect(k.out).to match /VERSION/
       expect(k.out).to match /kontena\/ingress-lb/
     end
 
     it 'shows a list of stacks filtered by description' do
-      k = run! 'kontena stack registry search balancer'
+      k = run! 'krates stack registry search balancer'
       expect(k.out).to match /kontena\/ingress-lb/
     end
   end
