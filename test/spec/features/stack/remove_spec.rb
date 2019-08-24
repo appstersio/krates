5b2ea2e06435caea591ba3e78d552b6f6d565c23
Pavel Tsurbeleu <pavel.tsurbeleu@me.com>
2019-08-23 19:51:37 -0700

    Fix up 'stack/remove' feature spec to refer new CLI

--- test/spec/features/stack/remove_spec.rb
+++ test/spec/features/stack/remove_spec.rb
@@ -2,33 +2,33 @@ require 'spec_helper'
 
 describe 'stack remove' do
   after(:each) do
-    run 'kontena stack rm --force simple'
+    run 'krates stack rm --force simple'
   end
 
   it "removes a stack" do
     with_fixture_dir("stack/simple") do
-      run! 'kontena stack install --no-deploy'
+      run! 'krates stack install --no-deploy'
     end
-    run! "kontena stack rm --force simple"
-    k = run "kontena stack show simple"
+    run! "krates stack rm --force simple"
+    k = run "krates stack show simple"
     expect(k.code).not_to eq(0)
   end
 
   it "removes multiple stacks" do
     with_fixture_dir("stack/simple") do
-      run! 'kontena stack install --no-deploy'
-      run! 'kontena stack install --no-deploy --name simple2'
+      run! 'krates stack install --no-deploy'
+      run! 'krates stack install --no-deploy --name simple2'
     end
-    run! "kontena stack rm --force simple simple2"
-    k = run "kontena stack show simple2"
+    run! "krates stack rm --force simple simple2"
+    k = run "krates stack show simple2"
     expect(k.code).not_to eq(0)
   end
 
   it "prompts without --force" do
     with_fixture_dir("stack/simple") do
-      run! 'kontena stack install --no-deploy'
+      run! 'krates stack install --no-deploy'
     end
-    k = kommando 'kontena stack rm simple', timeout: 5
+    k = kommando 'krates stack rm simple', timeout: 5
     k.out.on "To proceed, type" do
       sleep 0.5
       k.in << "simple\r"
@@ -40,27 +40,27 @@ describe 'stack remove' do
   context "for a stack that has dependencies" do
     before do
       with_fixture_dir("stack/depends") do
-        run! 'kontena stack install'
+        run! 'krates stack install'
       end
     end
 
     after do
-      run 'kontena stack rm --force twemproxy'
-      run 'kontena stack rm --force twemproxy-redis_from_yml'
-      run 'kontena stack rm --force twemproxy-twemproxy-redis_from_registry'
+      run 'krates stack rm --force twemproxy'
+      run 'krates stack rm --force twemproxy-redis_from_yml'
+      run 'krates stack rm --force twemproxy-twemproxy-redis_from_registry'
     end
 
     it 'removes all the dependencies' do
-      k = run! 'kontena stack ls -q'
+      k = run! 'krates stack ls -q'
       expect(k.out.split(/[\r\n]/)).to match array_including(
         'twemproxy-redis_from_registry',
         'twemproxy-redis_from_yml',
         'twemproxy'
       )
 
-      run! 'kontena stack rm --force twemproxy'
+      run! 'krates stack rm --force twemproxy'
 
-      k = run! 'kontena stack ls -q'
+      k = run! 'krates stack ls -q'
       expect(k.out).not_to match /twemproxy-redis_from_registry/
       expect(k.out).not_to match /twemproxy-redis_from_yml/
       expect(k.out).not_to match /twemproxy/
