8e9a6aa430d9f61267e886d59e26fa77cf517b2f
Pavel Tsurbeleu <pavel.tsurbeleu@me.com>
2019-08-23 21:14:46 -0700

    Fix up 'stack/shutdown' feature spec to refer new CLI

--- test/spec/features/stack/shutdown_spec.rb
+++ test/spec/features/stack/shutdown_spec.rb
@@ -1,28 +1,28 @@
 require 'net/http'
 
-describe 'kontena service shutdown' do
+describe 'krates service shutdown' do
   include ServiceHelper
 
   context 'for a http test service' do
     before(:all) do
       with_fixture_dir('stack/shutdown-test') do
-        run! 'kontena stack build --no-push'
+        run! 'krates stack build --no-push'
       end
     end
 
     context "using ungraceful shutdown to fail requests" do
       before(:all) do
         with_fixture_dir('stack/shutdown-test') do
-          run! "kontena stack install -n shutdown-test-fail -v trap=false"
+          run! "krates stack install -n shutdown-test-fail -v trap=false"
         end
       end
       after(:all) do
-        run! 'kontena stack rm --force shutdown-test-fail'
+        run! 'krates stack rm --force shutdown-test-fail'
       end
 
       describe "after re-deploying the server" do
         before do
-          run! 'kontena service deploy --force shutdown-test-fail/server'
+          run! 'krates service deploy --force shutdown-test-fail/server'
         end
 
         it "will result in client errors" do
@@ -36,16 +36,16 @@ describe 'kontena service shutdown' do
     context "using graceful shutdown to close the listening socket" do
       before(:all) do
         with_fixture_dir('stack/shutdown-test') do
-          run! "kontena stack install -n shutdown-test-graceful -v graceful_shutdown=true"
+          run! "krates stack install -n shutdown-test-graceful -v graceful_shutdown=true"
         end
       end
       after(:all) do
-        run! 'kontena stack rm --force shutdown-test-graceful'
+        run! 'krates stack rm --force shutdown-test-graceful'
       end
 
       describe "after re-deploying the server" do
         before do
-          run! 'kontena service deploy --force shutdown-test-graceful/server'
+          run! 'krates service deploy --force shutdown-test-graceful/server'
         end
 
         it "does not result in any client errors", :broken => true do
@@ -59,16 +59,16 @@ describe 'kontena service shutdown' do
     context "using graceful shutdown without a healthcheck" do
       before(:all) do
         with_fixture_dir('stack/shutdown-test') do
-          run! "kontena stack install -n shutdown-test-nohealthcheck -v graceful_shutdown=true -v healthcheck=false"
+          run! "krates stack install -n shutdown-test-nohealthcheck -v graceful_shutdown=true -v healthcheck=false"
         end
       end
       after(:all) do
-        run! 'kontena stack rm --force shutdown-test-nohealthcheck'
+        run! 'krates stack rm --force shutdown-test-nohealthcheck'
       end
 
       describe "after re-deploying the server" do
         before do
-          run! 'kontena service deploy --force shutdown-test-nohealthcheck/server'
+          run! 'krates service deploy --force shutdown-test-nohealthcheck/server'
         end
 
         it "does not result in any client errors", :broken => true do
@@ -82,16 +82,16 @@ describe 'kontena service shutdown' do
     context "using delayed shutdown without closing the listening socket" do
       before(:all) do
         with_fixture_dir('stack/shutdown-test') do
-          run! "kontena stack install -n shutdown-test-delay -v graceful_shutdown=false"
+          run! "krates stack install -n shutdown-test-delay -v graceful_shutdown=false"
         end
       end
       after(:all) do
-        run! 'kontena stack rm --force shutdown-test-delay'
+        run! 'krates stack rm --force shutdown-test-delay'
       end
 
       describe "after re-deploying the server" do
         before do
-          run! 'kontena service deploy --force shutdown-test-delay/server'
+          run! 'krates service deploy --force shutdown-test-delay/server'
         end
 
         it "does not result in any client errors", :broken => true do
