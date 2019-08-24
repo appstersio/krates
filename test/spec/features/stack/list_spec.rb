c794afdb0c635554ce6852f11acca3407bb1da3e
Pavel Tsurbeleu <pavel.tsurbeleu@me.com>
2019-08-23 19:49:43 -0700

    Fix up 'stack/list' feature spec to refer new CLI

--- test/spec/features/stack/list_spec.rb
+++ test/spec/features/stack/list_spec.rb
@@ -2,20 +2,20 @@ require 'spec_helper'
 
 describe 'stack list' do
   after do
-    run('kontena stack rm --force simple')
+    run('krates stack rm --force simple')
   end
 
   it "returns an empty list with headers" do
-    k = run! 'kontena stack ls'
+    k = run! 'krates stack ls'
     expect(k.out.lines.size).to eq(1)
     expect(k.out).to match(/NAME.*STACK.*STATE/)
   end
 
   it "returns an installed stack" do
     with_fixture_dir("stack/simple") do
-      run 'kontena stack install --no-deploy'
+      run 'krates stack install --no-deploy'
     end
-    k = run! 'kontena stack ls', env: { COLUMNS: "1000" }
+    k = run! 'krates stack ls', env: { COLUMNS: "1000" }
     expect(k.out.lines.size).to eq(2)
     expect(k.out.match(/simple.*test\/simple:.*initialized/)).to be_truthy
   end
@@ -23,15 +23,15 @@ describe 'stack list' do
   context 'quiet mode' do
     it "returns an installed stack name" do
       with_fixture_dir("stack/simple") do
-        run! 'kontena stack install --no-deploy'
+        run! 'krates stack install --no-deploy'
       end
-      k = run! 'kontena stack ls -q'
+      k = run! 'krates stack ls -q'
       expect(k.out.lines.size).to eq(1)
       expect(k.out.strip).to eq "simple"
     end
 
     it "returns nothing when there are no stacks" do
-      k = run! 'kontena stack ls -q'
+      k = run! 'krates stack ls -q'
       expect(k.out.lines.size).to eq(0)
     end
   end
