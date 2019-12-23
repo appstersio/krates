01a378945e97d93a3237e5bc82ff95416b3bcacc
Pavel Tsurbeleu <pavel.tsurbeleu@me.com>
2019-12-22 17:59:21 -0800

    Build: Fix issue with accidentally removed 'bundle install' step

--- build/travis/test.sh
+++ build/travis/test.sh
@@ -3,6 +3,6 @@ set -ue
 
  # CVE-2018-1000539 is a vulnerability in JSON-JWT, used by ancient Acme::Client for sending requests, not for validating.
 cd $TEST_DIR && \
-  bundle config set path 'vendor/bundle' && \
+  bundle config set path 'vendor/bundle' && bundle install && \
   bundle audit check --update --ignore CVE-2018-1000539 CVE-2019-16779 && \
   bundle exec rspec spec/
