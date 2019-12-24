219b1f86e4a08f718cc9a5f1bdae9f774af46a5e
Pavel Tsurbeleu <pavel.tsurbeleu@me.com>
2019-12-23 17:21:29 -0800

    E2E: Fix issues with 'log-driver' in 'features/service/logs_spec.rb'

--- test/spec/features/service/logs_spec.rb
+++ test/spec/features/service/logs_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
 describe 'service logs' do
-  before(:each) do
-    run("kontena service create --instances 2 test-1 redis:3.0")
+  before(:all) do
+    run("kontena service create --log-driver journald --instances 2 test-1 redis:3.0")
     run("kontena service deploy test-1")
-    sleep 1
+    sleep 5
   end
 
-  after(:each) do
+  after(:all) do
     run("kontena service rm --force test-1")
   end
 
