04ea2c526870cf726f5ea00bf7c9a19f2d759862
Pavel Tsurbeleu <pavel.tsurbeleu@me.com>
2019-12-26 20:31:58 -0800

    Another attempt to fix a broken test in Travis CI environment

--- test/spec/features/secrets/envlimit_spec.rb
+++ test/spec/features/secrets/envlimit_spec.rb
@@ -3,23 +3,24 @@ require 'spec_helper'
 describe 'secret env size limits' do
   context 'with a deployed stack' do
     before(:each) do
-      run 'kontena stack rm --force secrets-envsize'
+      run '$ kontena stack rm --force secrets-envsize'
       with_fixture_dir("secrets") do
-        run! 'kontena stack install -v secret_count=64 envsize.yaml'
+        run! '$ kontena stack install -v secret_count=64 envsize.yaml'
       end
     end
 
     after(:each) do
-      run 'kontena stack rm --force secrets-envsize'
+      run '$ kontena stack rm --force secrets-envsize'
     end
 
     it 'fails to upgrade with too many secrets, without killing the deployed container' do
       cid = container_id('secrets-envsize.test-1')
 
       with_fixture_dir("secrets") do
-        expect {
-          `kontena stack upgrade -v secret_count=128 secrets-envsize envsize.yaml`
-        }.to output(/Kontena::Models::ServicePod::ConfigError: Env SECRETS is too large at \d+ bytes/).to_stderr_from_any_process
+        k = run "$ kontena stack upgrade -v secret_count=128 secrets-envsize envsize.yaml"
+        puts k.out
+        expect(k.code).to_not eq 0
+        expect(k.out).to match /Kontena::Models::ServicePod::ConfigError: Env SECRETS is too large at \d+ bytes/
       end
 
       # still running?
