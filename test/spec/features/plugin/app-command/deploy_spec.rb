f6a902f79b009740b5cae9edfc3e292e1726ba8b
Pavel Tsurbeleu <pavel.tsurbeleu@me.com>
2019-12-30 00:54:00 -0800

    Exclude 'plugin/app-command' specs from coverage since they reference obsolete feature

--- test/spec/features/plugin/app-command/deploy_spec.rb
+++ test/spec/features/plugin/app-command/deploy_spec.rb
@@ -1,23 +1,23 @@
 require 'spec_helper'
-
-describe 'app subcommand' do
+# NOTE: These tests are obsolete but might be useful for future reference
+describe 'app subcommand', :skip => 'OBSOLETE' do
   context 'with the app-command plugin', subcommand: :app do
     describe 'app deploy' do
       it 'deploys a simple app' do
         with_fixture_dir('app/simple') do
-          k = run('kontena app deploy')
+          k = run('krates app deploy')
           k.wait
           expect(k.code).to eq(0)
           
           # hack to ensure that the app services get cleaned up before the next spec runs
-          run! 'kontena service update --stop-timeout=1s simple-lb'
-          run! 'kontena service deploy --force simple-lb'
+          run! 'krates service update --stop-timeout=1s simple-lb'
+          run! 'krates service deploy --force simple-lb'
 
-          k = run!('kontena service ls')
+          k = run!('krates service ls')
           %w(lb nginx redis).each do |service|
             expect(k.out).to match(/simple-#{service}/)
           end
-          run!('kontena app rm --force')
+          run!('krates app rm --force')
         end
       end
     end
