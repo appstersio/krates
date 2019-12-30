5433f19dfb1f0dcda46f3e85cc67ea376fbdc60c
Pavel Tsurbeleu <pavel.tsurbeleu@me.com>
2019-12-30 00:20:41 -0800

    Krates: Make 'features/grid', 'features/master' and 'features/nodes' specs to pass

--- test/spec/features/grid/trusted_subnet_spec.rb
+++ test/spec/features/grid/trusted_subnet_spec.rb
@@ -1,26 +1,26 @@
 describe 'grid trusted-subnet' do
   describe 'list' do
     it 'lists subnets' do
-      run! "kontena grid trusted-subnet ls"
+      run! "krates grid trusted-subnet ls"
       # TODO output check
     end
   end
 
   describe 'add' do
     after(:each) do
-      run "kontena grid trusted-subnet rm --force 192.168.22.0/24"
+      run "krates grid trusted-subnet rm --force 192.168.22.0/24"
     end
 
     it 'adds a subnet' do
-      run! "kontena grid trusted-subnet add 192.168.22.0/24"
+      run! "krates grid trusted-subnet add 192.168.22.0/24"
       # TODO result check
     end
   end
 
   describe 'remove' do
     it 'removes subnet' do
-      run! "kontena grid trusted-subnet add 192.168.23.0/24"
-      run! "kontena grid trusted-subnet rm --force 192.168.23.0/24"
+      run! "krates grid trusted-subnet add 192.168.23.0/24"
+      run! "krates grid trusted-subnet rm --force 192.168.23.0/24"
       # TODO result check
     end
   end
