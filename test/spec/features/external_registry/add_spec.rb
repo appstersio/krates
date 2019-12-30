30727643cc23c30426e93f743d96e7a35b3586b3
Pavel Tsurbeleu <pavel.tsurbeleu@me.com>
2019-12-30 00:48:57 -0800

    Krates: Fix up 'external_registry' feature specs to refer new CLI

--- test/spec/features/external_registry/add_spec.rb
+++ test/spec/features/external_registry/add_spec.rb
@@ -2,35 +2,35 @@ require 'spec_helper'
 
 describe 'external-registry add' do
   after(:each) do
-    run 'kontena external-registry rm --force registry.domain.com'
+    run 'krates external-registry rm --force registry.domain.com'
   end
 
   it 'adds a new external-registry' do
-    run! 'kontena external-registry add -u foo -e foo@no.email -p secret https://registry.domain.com/'
-    k = run! 'kontena external-registry ls'
+    run! 'krates external-registry add -u foo -e foo@no.email -p secret https://registry.domain.com/'
+    k = run! 'krates external-registry ls'
     expect(k.out.include?('registry.domain.com'))
   end
 
   it 'adds a new external-registry without protocol' do
-    run! 'kontena external-registry add -u foo -e foo@no.email -p secret registry.domain.com'
-    k = run! 'kontena external-registry ls'
+    run! 'krates external-registry add -u foo -e foo@no.email -p secret registry.domain.com'
+    k = run! 'krates external-registry ls'
     expect(k.out.include?('registry.domain.com'))
   end
 
   it 'requires -u option' do
-    k = run 'kontena external-registry add https://foo.domain.com'
+    k = run 'krates external-registry add https://foo.domain.com'
     expect(k.code).not_to eq(0)
     expect(k.out.include?("'-u' is required"))
   end
 
   it 'requires -e option' do
-    k = run 'kontena external-registry -u user add https://foo.domain.com'
+    k = run 'krates external-registry -u user add https://foo.domain.com'
     expect(k.code).not_to eq(0)
     expect(k.out.include?("'-e' is required"))
   end
 
   it 'requires -p option' do
-    k = run 'kontena external-registry -u user -e foo@email.com add https://foo.domain.com'
+    k = run 'krates external-registry -u user -e foo@email.com add https://foo.domain.com'
     expect(k.code).not_to eq(0)
     expect(k.out.include?("'-p' is required"))
   end
