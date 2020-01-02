2ba0b3f57da44edc629e3e9b552e5d2d5dae50bd
Pavel Tsurbeleu <pavel.tsurbeleu@me.com>
2020-01-01 23:34:03 -0800

    Fix up 'container/logs' feature spec to refer new CLI

--- test/spec/features/container/logs_spec.rb
+++ test/spec/features/container/logs_spec.rb
@@ -3,12 +3,12 @@ describe 'container logs' do
     id = container_id('krates-worker')
     expect(id).not_to be_nil
 
-    k = kommando("kontena container logs #{id}")
+    k = kommando("krates container logs #{id}")
     expect(k.run).to be_truthy
   end
 
   it 'returns error if container does not exist' do
-    k = run("kontena container logs invalid-id")
+    k = run("krates container logs invalid-id")
     expect(k.code).to eq(1)
   end
 end
