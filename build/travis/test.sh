5afa459c5f3e149c2d0889571cee519c3de5f62a
Pavel Tsurbeleu <pavel.tsurbeleu@me.com>
2020-01-12 14:02:40 -0800

    Makefile: Enable tracing with specifying 'TRACE=1' explicilty in the command

--- build/travis/test.sh
+++ build/travis/test.sh
@@ -3,8 +3,15 @@ set -ue
 
 echo ":: TEST ::"
  # CVE-2018-1000539 is a vulnerability in JSON-JWT, used by ancient Acme::Client for sending requests, not for validating.
-pushd $TEST_DIR && \
-  bundle install && \
-  bundle audit check --update --ignore CVE-2018-1000539 CVE-2019-16779 && \
+pushd $TEST_DIR
+bundle install
+
+if [ "$TRACE" = "1" ];
+then
+  /bin/bash
+else
+  bundle audit check --update --ignore CVE-2018-1000539 CVE-2019-16779
   bundle exec rspec spec/
+fi
+
 echo
