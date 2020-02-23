87796ccdb648aadf60f0f95ed319ed5e5aabc4bd
Pavel Tsurbeleu <pavel.tsurbeleu@me.com>
2020-02-23 12:23:32 -0800

    Worker: Support for a grace period (5 minutes) in ContainerCleanupWorker and avoid aggressive cleanup by default

--- agent/lib/kontena/workers/container_cleanup_worker.rb
+++ agent/lib/kontena/workers/container_cleanup_worker.rb
@@ -4,7 +4,8 @@ module Kontena::Workers
       include Kontena::Logging
   
       CLEANUP_INTERVAL = 60
-      CLEANUP_DELAY = (60*60)
+      # Remove only containers exited 5+ minutes ago
+      GRACE_PERIOD_IN_MINUTES = 5
   
       ##
       # @param [Boolean] autostart
@@ -24,12 +25,32 @@ module Kontena::Workers
         containers = Docker::Container.all(all: true, filters: { status: ["exited"]}.to_json)
         containers.each do |container|
           begin
+            # Skip containers within the grace period
+            next if minutes_ago(container.info['Status']) < GRACE_PERIOD_IN_MINUTES
+            # Remove containers outside of the grace period
             container.remove
             info "Removed exited container: #{container.id}"
           rescue
             error "Failed to remove exited container: #{container.id}"
           end
         end
+      end
+
+      def minutes_ago(status)
+        # Set initial state and match minutes
+        m = /(\d+) minute/.match(status)
+        # Convert from minutes
+        return m[1].to_i if m
+        # Match hours
+        h = /(\d+) hour/.match(status)
+        # Convert from hours
+        return h[1].to_i * 60 if h
+        # Match days
+        d = /(\d+) day/.match(status)
+        # Convert from days
+        return d[1].to_i * 1440 if d
+        # By default it is 0
+        return 0
       end
     end
 end
