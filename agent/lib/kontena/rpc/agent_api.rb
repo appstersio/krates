58d6d280b0f2f8d847f76a47de6ec65c76b7d54e
Pavel Tsurbeleu <pavel.tsurbeleu@me.com>
2020-07-04 12:46:10 -0700

    AgentApi: Discontinue 'master_info' since auto-update relies on 'always latest' approach

--- agent/lib/kontena/rpc/agent_api.rb
+++ agent/lib/kontena/rpc/agent_api.rb
@@ -5,29 +5,10 @@ module Kontena
     class AgentApi
 
       # @param [Hash] data
-      def master_info(data)
-        Celluloid::Notifications.publish('websocket:connected', {master: data})
-        update_version(data['version']) if data['version']
-        {}
-      end
-
-      # @param [Hash] data
       def node_info(data)
         node = Node.new(data)
         Celluloid::Notifications.publish('agent:node_info', node)
         {}
-      end
-
-      private
-
-      # @param [String] version
-      def update_version(version)
-        env_file = '/etc/kontena.env'
-        if File.exist?(env_file)
-          env = File.read(env_file)
-          env.gsub!(/^KONTENA_VERSION=.+$/, "KONTENA_VERSION=#{version}")
-          File.write(env_file, env)
-        end
       end
     end
   end
--- agent/spec/lib/kontena/rpc/agent_api_spec.rb
+++ agent/spec/lib/kontena/rpc/agent_api_spec.rb
@@ -4,15 +4,6 @@ describe Kontena::Rpc::AgentApi do
   before(:each) { Celluloid.boot }
   after(:each) { Celluloid.shutdown }
 
-  describe '#master_info' do
-    it 'publishes event' do
-      info = {'version' => '0.10.0'}
-      expect(Celluloid::Notifications).to receive(:publish).with('websocket:connected', {master: info})
-      subject.master_info(info)
-      sleep 0.01
-    end
-  end
-
   describe '#node_info' do
     it 'publishes event' do
       info = {'id' => 'xyz'}
